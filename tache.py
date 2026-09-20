e23a33bf0e84e590f36d5e7d276775076a5fecf6
Jorel-code <jorelbrayan1@gmail.com>
2026-09-20 17:45:10 +0100

    CRUD complet des tache et sauvegarde dans un fichier json

--- tache.py
+++ tache.py
@@ -16,3 +16,49 @@ def changer_status():
         if tache["id"] == id_tache:
             tache['statut'] = 'terminée'
             print(f"Tache {id_tache} marquée comme terminée")
+
+def supprimer_tache():
+    id_tache = int(input("Entrez l'ID de la tache: "))
+    for tache in taches:
+        if tache['id'] == id_tache:
+            taches.remove(tache)
+    print(f"Tache {id_tache} supprimée")
+
+def sauvegarder():
+    with open("taches.json", "w", encoding = "utf-8") as f:
+        json.dump(taches, f, indent = 2, ensure_ascii = False) 
+
+def charger():
+    global taches
+    try:
+        with open("taches.json", "r", encoding = "utf-8") as f:
+            taches = json.load(f)
+    except FileNotFoundError:
+        taches = []
+
+def launch():
+    charger()
+    while True:
+        print( "\n== MENU ==")
+        print(" Entrez votre choix")
+        print("  1. Ajouter une tache")
+        print("  2. Lister les taches")
+        print("  3. Marquer une tache comme terminé")
+        print("  4. Supprimer une tache")
+        print("  5. Quitter")
+        choix = input()
+        if choix == "1":
+            ajout_tache()
+            sauvegarder()
+        elif  choix == "2":
+            liste_taches()
+        elif choix == "3":
+            changer_status()
+        elif choix == "4":
+            supprimer_tache()
+        elif choix == "5":
+            sauvegarder()
+            print("Au revoir")
+            break
+
+launch()
